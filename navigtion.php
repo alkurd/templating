f286efd5acce84e71e5a8ff9954982de8a1746ae
alkurd <yalkerdi@gmail.com>
2025-05-19 14:48:51 +0200

    Added files for exercise 3 templating

---
+++ navigtion.php
@@ -0,0 +1,9 @@
+<nav>
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="projectien.php">projectien</a></li>
+        <li><a href="contact.php">contact</a></li>
+    </ul>
+</nav>
+
+<footer>
